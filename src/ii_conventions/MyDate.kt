857deafca2f1986f6a9db7a60f32c9426c7d07eb
Nathan Schwermann <nschwermann@pinsightmedia.com>
2015-06-08 21:40:00 -0500

    next session

--- src/ii_conventions/MyDate.kt
+++ src/ii_conventions/MyDate.kt
@@ -1,6 +1,13 @@
 package ii_conventions
 
-data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int)
+data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int) : Comparable<MyDate> {
+
+    override fun compareTo(other: MyDate): Int {
+        if(year != other.year) return year.compareTo(other.year)
+        if(month != other.month) return month.compareTo(other.month)
+        return dayOfMonth.compareTo(other.dayOfMonth)
+    }
+}
 
 enum class TimeInterval {
     DAY
@@ -8,4 +15,23 @@ enum class TimeInterval {
     YEAR
 }
 
-class DateRange(public val start: MyDate, public val end: MyDate)
+//        Make class DateRange implement Iterable<MyDate>.
+//        Use object expression to implement Iterator<MyDate>.
+//        Use an utility function 'MyDate.nextDay()'.
+
+class DateRange(public val start: MyDate, public val end: MyDate) : Iterable<MyDate> {
+
+    override fun iterator() = object : Iterator<MyDate> {
+
+        var current : MyDate = start
+
+        override fun next() : MyDate {
+            val result = current
+            current = current.nextDay()
+            return result
+        }
+
+        override fun hasNext() : Boolean = current <= end
+    }
+
+}
