3cf891e5d949cf586c91da94a5a49066dfe5c978
Nathan Schwermann <nschwermann@pinsightmedia.com>
2015-06-10 23:27:57 -0500

    session 5

--- src/ii_conventions/MyDate.kt
+++ src/ii_conventions/MyDate.kt
@@ -7,6 +7,10 @@ data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int) : Comparab
         if(month != other.month) return month.compareTo(other.month)
         return dayOfMonth.compareTo(other.dayOfMonth)
     }
+
+    //To make '..' work add 'MyDate.rangeTo()' extension function returning DateRange.
+    //Add all changes to the file MyDate.kt.
+    fun rangeTo(end : MyDate) = DateRange(this, end)
 }
 
 enum class TimeInterval {
@@ -19,7 +23,8 @@ enum class TimeInterval {
 //        Use object expression to implement Iterator<MyDate>.
 //        Use an utility function 'MyDate.nextDay()'.
 
-class DateRange(public val start: MyDate, public val end: MyDate) : Iterable<MyDate> {
+
+class DateRange(public override val start: MyDate, public override val end: MyDate) : Iterable<MyDate>, Range<MyDate> {
 
     override fun iterator() = object : Iterator<MyDate> {
 
@@ -34,4 +39,11 @@ class DateRange(public val start: MyDate, public val end: MyDate) : Iterable<MyD
         override fun hasNext() : Boolean = current <= end
     }
 
+//    Uncomment the commented line.
+//    For now being in interval is checked with the function 'fun <T> Iterable<T>.contains(T): Boolean'
+//    which is not optimal (you can check the boundaries instead of iterating over the whole range).
+//    Make class DateRange implement kotlin.Range<MyDate>.
+    override fun contains(item: MyDate): Boolean {
+        return item >= start && item <= end
+    }
 }
