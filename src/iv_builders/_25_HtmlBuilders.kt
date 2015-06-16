a6b11e218f5466a23b393cf3ee358da87a29837d
Nathan Schwermann <nschwermann@pinsightmedia.com>
2015-06-15 20:35:55 -0500

    session 10

--- src/iv_builders/_25_HtmlBuilders.kt
+++ src/iv_builders/_25_HtmlBuilders.kt
@@ -21,18 +21,24 @@ fun renderProductTable(): String {
     return html {
         table {
             tr {
-                td {
+                td(getTitleColor()) {
                     text("Product")
                 }
-                td {
+                td(getTitleColor()) {
                     text("Price")
                 }
-                td {
+                td(getTitleColor()) {
                     text("Popularity")
                 }
             }
             val products = getProducts()
-            todoTask25()
+            products.forEachIndexed { index, product ->
+                tr {
+                  td(getCellColor(1, index)){text(product.description)}
+                  td(getCellColor(2, index)){text(product.price)}
+                  td(getCellColor(3, index)){text(product.price)}
+                }
+            }
         }
     }.toString()
 }
